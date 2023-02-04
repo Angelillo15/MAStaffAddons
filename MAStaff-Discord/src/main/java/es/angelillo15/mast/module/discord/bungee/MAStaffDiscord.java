cf846c97149ffee9befcc7dd4c65b6e1e01277f7
Angelillo15 <contact@angelillo15.es>
2023-02-04 20:01:31 +0100

    Added new listeners conditions

--- MAStaff-Discord/src/main/java/es/angelillo15/mast/module/discord/bungee/MAStaffDiscord.java
+++ MAStaff-Discord/src/main/java/es/angelillo15/mast/module/discord/bungee/MAStaffDiscord.java
@@ -2,8 +2,12 @@ package es.angelillo15.mast.module.discord.bungee;
 
 import es.angelillo15.mast.api.addons.bungee.MAStaffBungeeAddon;
 import es.angelillo15.mast.api.chat.api.ChatColor;
+import es.angelillo15.mast.module.discord.bungee.listener.OnJoin;
+import es.angelillo15.mast.module.discord.bungee.listener.OnLeave;
 import es.angelillo15.mast.module.discord.bungee.listener.OnStaffChat;
+import es.angelillo15.mast.module.discord.bungee.listener.OnSwitch;
 import es.angelillo15.mast.module.discord.common.Config;
+import net.md_5.bungee.api.plugin.Listener;
 
 public class MAStaffDiscord extends MAStaffBungeeAddon {
     @Override
@@ -15,7 +19,14 @@ public class MAStaffDiscord extends MAStaffBungeeAddon {
     }
 
     public void loadListeners() {
-        getMastaffInstance().getProxy().getPluginManager().registerListener(getMastaffInstance(), new OnStaffChat());
+        if(Config.StaffChat.isEnabled()) registerListener(new OnStaffChat());
+        if(Config.StaffSwitch.isEnabled()) registerListener(new OnSwitch());
+        if(Config.StaffJoin.isEnabled()) registerListener(new OnJoin());
+        if(Config.StaffLeave.isEnabled()) registerListener(new OnLeave());
+    }
+
+    public void registerListener(Listener listener) {
+        getMastaffInstance().getProxy().getPluginManager().registerListener(getMastaffInstance(), listener);
     }
     @Override
     public void onDisable() {
