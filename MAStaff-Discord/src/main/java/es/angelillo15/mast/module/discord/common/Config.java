09c1ab1a321dadc51941366ef205bcfd52791930
Angelillo15 <contact@angelillo15.es>
2023-02-11 14:42:32 +0100

    Added new config fields

--- MAStaff-Discord/src/main/java/es/angelillo15/mast/module/discord/common/Config.java
+++ MAStaff-Discord/src/main/java/es/angelillo15/mast/module/discord/common/Config.java
@@ -92,5 +92,89 @@ public class Config {
             return config.getString("StaffSwitch.message");
         }
     }
+
+    public static class StaffEnable {
+        public static boolean isEnabled() {
+            return config.getBoolean("StaffEnable.enable");
+        }
+        public static String getWebhookUrl() {
+            return config.getString("StaffEnable.webhook");
+        }
+
+        public static String getWebhookName() {
+            return config.getString("StaffEnable.name");
+        }
+
+        public static String getWebhookAvatar() {
+            return config.getString("StaffEnable.avatar");
+        }
+
+        public static String getWebhookMessage() {
+            return config.getString("StaffEnable.message");
+        }
+    }
+
+    public static class StaffDisable {
+        public static boolean isEnabled() {
+            return config.getBoolean("StaffDisable.enable");
+        }
+        public static String getWebhookUrl() {
+            return config.getString("StaffDisable.webhook");
+        }
+
+        public static String getWebhookName() {
+            return config.getString("StaffDisable.name");
+        }
+
+        public static String getWebhookAvatar() {
+            return config.getString("StaffDisable.avatar");
+        }
+
+        public static String getWebhookMessage() {
+            return config.getString("StaffDisable.message");
+        }
+    }
+
+    public static class Freeze {
+        public static boolean isEnabled() {
+            return config.getBoolean("Freeze.enable");
+        }
+        public static String getWebhookUrl() {
+            return config.getString("Freeze.webhook");
+        }
+
+        public static String getWebhookName() {
+            return config.getString("Freeze.name");
+        }
+
+        public static String getWebhookAvatar() {
+            return config.getString("Freeze.avatar");
+        }
+
+        public static String getWebhookMessage() {
+            return config.getString("Freeze.message");
+        }
+    }
+
+    public static class Unfreeze {
+        public static boolean isEnabled() {
+            return config.getBoolean("Unfreeze.enable");
+        }
+        public static String getWebhookUrl() {
+            return config.getString("Unfreeze.webhook");
+        }
+
+        public static String getWebhookName() {
+            return config.getString("Unfreeze.name");
+        }
+
+        public static String getWebhookAvatar() {
+            return config.getString("Unfreeze.avatar");
+        }
+
+        public static String getWebhookMessage() {
+            return config.getString("Unfreeze.message");
+        }
+    }
 }
 
